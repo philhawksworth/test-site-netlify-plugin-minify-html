11a65d46dd62782f06df47c66022d9cc465e765b
Phil Hawksworth <phil@netlify.com>
2020-02-06 15:34:23 +0000

    S simple build with pluign config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addLayoutAlias('default', 'base.njk');
+  eleventyConfig.addPassthroughCopy("site/css");
+  eleventyConfig.addPassthroughCopy("site/images");
 
   return {
     dir: {
---
+++ README.md
@@ -0,0 +1,6 @@
+# Test site: netlify-plugin-html-minifier
+
+This site only exists to test a Netlify Build Plugin.
+
+Learn more about the [netlify-plugin-html-minifier](https://github.com/philhawksworth/netlify-plugin-minify-html)
+
